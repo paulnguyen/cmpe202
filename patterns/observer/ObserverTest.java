9003c7ad245aff3e489a1322789041561a1c71fc
Paul Nguyen <paul.nguyen@sjsu.edu>
2016-03-13 20:03:12 -0700

    no message

--- patterns/observer/ObserverTest.java
+++ patterns/observer/ObserverTest.java
@@ -51,6 +51,8 @@ public class ObserverTest
         s.attach(p);
         s.attach(o);
         s.setState("The New iPad is out today");
+        p.showState();
+        o.showState();
         s.setState("Hey, Its Friday!");
         p.showState();
         o.showState();
