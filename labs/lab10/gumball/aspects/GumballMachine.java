6c2906359e591264f60856cb8ffa75f3ed547cf7
paulnguyen <paul.nguyen@sjsu.edu>
2019-03-22 23:16:58 -0700

    added a more complete bytecode dump

--- labs/lab10/gumball/aspects/GumballMachine.java
+++ labs/lab10/gumball/aspects/GumballMachine.java
@@ -1,5 +1,6 @@
 
 import java.lang.reflect.* ;
+import java.util.* ;
 
 public class GumballMachine {
 
@@ -60,17 +61,69 @@ public class GumballMachine {
 	// Main Class - Dump Metadata
 	public static void main( String args[ ] )
 	{
-		System.out.println( "***** Class Bytecode Dump *****" ) ;
+
+		System.out.println( "\n\n***** Class Bytecode Dump *****" ) ;
 		GumballMachine m = new GumballMachine(10) ;
 		Class gmClass = m.getClass() ;
-		Method gmMethods[] = gmClass.getMethods() ;
-		for ( int i=0; i <gmMethods.length; i++ )
-		{
-			Method theMethod = gmMethods[i] ;
-			String method = theMethod.toString() ;
-			System.out.println( method ) ;
-		}
+
+        System.out.println( "\nFIELDS:" ) ;
+        Field[] fields = gmClass.getDeclaredFields();
+        for (Field f : fields) {
+
+        	int mods = f.getModifiers();
+ 
+        	if ( Modifier.isPublic(mods) )
+        		System.out.format( "  public" ) ;
+        	else if ( Modifier.isPrivate(mods) )
+        		System.out.format( "  private" ) ;
+        	else if ( Modifier.isProtected(mods) )
+        		System.out.format( "  protected" ) ;
+ 
+        	if ( Modifier.isStatic(mods) )
+        		System.out.format( " static") ;
+
+        	System.out.format( " %s %s\n", f.getType(), f.getName() ) ;
+
+        }
+
+        System.out.println( "\nMETHODS:" ) ;
+        Method gmMethods[] = gmClass.getMethods() ;
+        for ( int i=0; i <gmMethods.length; i++ )
+        {
+            Method theMethod = gmMethods[i] ;
+            String method = theMethod.toString() ;
+            System.out.format( "  %s\n", method ) ;
+        }
+
+      	System.out.println( "\nINTERFACES:" ) ;
+        Type[] intfs = gmClass.getGenericInterfaces();
+        if (intfs.length != 0) {
+        for (Type intf : intfs)
+            System.out.format("  %s%n", intf.toString());
+        System.out.format("%n");
+        } else {
+            System.out.format("  -- No Implemented Interfaces --%n%n");
+        }
+
+        System.out.println( "\nEXTENDS:" ) ;
+        List<Class> l = new ArrayList<Class>();
+        printAncestor(gmClass, l);
+        if (l.size() != 0) {
+        for (Class<?> cl : l)
+            System.out.format("  %s%n", cl.getCanonicalName());
+        System.out.format("%n");
+        } else {
+            System.out.format("  -- No Super Classes --%n%n");
+        }        
 		
 	}
+
+	private static void printAncestor(Class<?> c, List<Class> l) {
+        Class<?> ancestor = c.getSuperclass();
+        if (ancestor != null) {
+            l.add(ancestor);
+            printAncestor(ancestor, l);
+        }
+    }
 	
 }
