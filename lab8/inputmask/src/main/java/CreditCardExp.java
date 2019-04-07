9f68d40eeddf8a8fc5bce331176e359e09df7e6a
tigon1508 <chi.t.tran@sjsu.edu>
2019-04-06 23:51:53 -0700

    Commit

--- lab8/inputmask/src/main/java/CreditCardExp.java
+++ lab8/inputmask/src/main/java/CreditCardExp.java
@@ -5,6 +5,10 @@ public class CreditCardExp implements IDisplayComponent, IKeyEventHandler
 
 	private IKeyEventHandler nextHandler ;
 	private String date = "" ;
+	private ConcreteDecoratorSlash cds;
+	private ConcreteDecoratorSpace cds1;
+	private String date1 = "";
+	private String date2 = "";
 
     public void setNext( IKeyEventHandler next) {
     	this.nextHandler = next ;
@@ -20,6 +24,12 @@ public class CreditCardExp implements IDisplayComponent, IKeyEventHandler
 	public void key(String ch, int cnt) {
 		if ( cnt >= 17 && cnt <= 20  )
 			date += ch ;
+		if (date.length() == 4){
+			date1 = cds.addCharacter(date.substring(0, 1));
+			date2 = cds1.addCharacter(date.substring(2, 3));
+
+			date = date1 + date2;
+		}
 		else if ( nextHandler != null )
 			nextHandler.key(ch, cnt) ;
 	}	
--- lab8/inputmask/src/main/java/CreditCardNum.java
+++ lab8/inputmask/src/main/java/CreditCardNum.java
@@ -5,6 +5,17 @@ public class CreditCardNum implements IDisplayComponent, IKeyEventHandler
 
 	private IKeyEventHandler nextHandler ;
 	private String number = "" ;
+	private String number1 = "";
+	private String number2 = "";
+	private String number3 = "";
+	private String number4 = "";
+
+	private ConcreteDecoratorSpace cds;
+
+	public CreditCardNum ()
+	{
+     	cds = new ConcreteDecoratorSpace();
+	}
 
     public void setNext( IKeyEventHandler next) {
     	this.nextHandler = next ;
@@ -18,8 +29,18 @@ public class CreditCardNum implements IDisplayComponent, IKeyEventHandler
 	}	
 
 	public void key(String ch, int cnt) {
-		if ( cnt <= 16 )
-			number += ch ;
+		if ( cnt <= 16 ) {
+			number += ch;
+			if (number.length() == 16){
+				number1 = cds.addCharacter(number.substring(0, 3));
+				number2 = cds.addCharacter(number.substring(4, 7));
+				number3 = cds.addCharacter(number.substring(8, 11));
+				number4 = cds.addCharacter(number.substring(12, 15));
+
+				number = number1 + number2 + number3 + number4;
+			}
+
+		}
 		else if ( nextHandler != null )
 			nextHandler.key(ch, cnt) ;
 	}	
